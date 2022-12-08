257393c630377f6fd333b97011bb587463ef26b5
Alf-caput <alfre.roble.aba@gmail.com>
2022-12-08 13:33:20 +0100

    descripcion inicial P1

---
+++ PR2_P1_Alfredo_Ruben.c
@@ -0,0 +1,12 @@
+/*=============================================================================
+ |   Asignatura:  Estructura de datos y algoritmos
+ |      Autores:  Alfredo Robledano Abasolo, Ruben Sierra Serrano
+ |     Lenguaje:  C
+ |        Clase:  2-B
+ |        Fecha:  08-12-2022
+ +-----------------------------------------------------------------------------
+ |  Descripcion:  Implementacion de tres metodos de ordenacion, para ordenar
+ |                numeros, cadenas de caracteres (alfabeticamente) y otro
+ |      Entrada:  Metodo que desea aplicar el usuario y elementos a ordenar
+ |       Salida:  Array o TAD ordenado
+ *===========================================================================*/
