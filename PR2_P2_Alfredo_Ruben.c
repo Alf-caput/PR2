fe6950d0cceba3ba95884898ac96bd43763c0bab
Alf-caput <alfre.roble.aba@gmail.com>
2022-12-08 13:33:48 +0100

    descripcion inicial P2

---
+++ PR2_P2_Alfredo_Ruben.c
@@ -0,0 +1,11 @@
+/*=============================================================================
+ |   Asignatura:  Estructura de datos y algoritmos
+ |      Autores:  Alfredo Robledano Abasolo, Ruben Sierra Serrano
+ |     Lenguaje:  C
+ |        Clase:  2-B
+ |        Fecha:  08-12-2022
+ +-----------------------------------------------------------------------------
+ |  Descripcion:  Implementacion de arboles binarios
+ |      Entrada:  Numeros o caracteres introducidos por el ususario
+ |       Salida:  Arbol binario
+ *===========================================================================*/
